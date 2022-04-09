774fe66ffc6e56765e24b70bebff8df2f78ea07b
3takita <98113361+3takita@users.noreply.github.com>
2022-04-08 18:39:25 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import CoverPage from "./components/CoverPage";
+import MainPage from "./components/MainPage";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CoverPage />
+      <MainPage />
+    </div>
+  );
+}
